366963575820a1fbace0996f4d446d42e3cb9420
睿麟 張 <jovi920204@gmail.com>
2022-04-21 14:30:56 +0800

    Jovi is testing

--- BigNumberCalc/main.cpp
+++ BigNumberCalc/main.cpp
@@ -11,6 +11,9 @@ int main(int argc, char* argv[])
 	BigNumber floatb("-3456.789");
 	BigNumber floatc("0.000123");
 	BigNumber floatd("-0.789");
+	BigNumber floatd("1234");
+
+	// Jovi is here
 	cout << inta + inta;
 	return 0;
 }
