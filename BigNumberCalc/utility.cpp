409381bf08532f058c6e0dc9082283f9eaa6bbdd
CCcat <linnemo920317@gmail.com>
2022-04-23 15:06:04 +0800

    comment

--- BigNumberCalc/utility.cpp
+++ BigNumberCalc/utility.cpp
@@ -3,4 +3,11 @@
 BigNumber basicAdd(const BigNumber& a, const BigNumber& b)
 {
 	// TODO
+	// integer addition (++, +-, -+, --)
+}
+
+BigNumber basicSub(const BigNumber& a, const BigNumber& b)
+{
+	// TODO
+	// integer substruction (++, +-, -+, --)
 }
